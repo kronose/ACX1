9486c732ae8e4f80fff7e74ca6878d15dc7aa197
kronose <78413714+kronose@users.noreply.github.com>
2021-02-03 13:34:08 +0100

    Create read_files.py

---
+++ read_files.py
@@ -0,0 +1,14 @@
+print ("this is file reader")
+def read_it(filename):
+	with open(filename , "r") as f
+	#f=open(filename,"r")
+	print(f.read())
+	#r=f.read()
+l=input("The name of your file in a between indented commas :")
+'''
+for example
+"examlpe.py"
+'''
+read_it(l)  
+
+
